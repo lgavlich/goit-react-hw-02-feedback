f46ae3a558c9e659f152534b817674c9fc994280
lgavlich <lgavlich@gmail.com>
2021-10-03 23:25:01 +0300

    Update Statistics.js

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -4,12 +4,12 @@ import Notification from "../Notification";
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return total ? (
-    <ul class="statList">
+    <ul>
       <li>Good:{good}</li>
       <li>Neutral:{neutral}</li>
       <li>Bad:{bad}</li>
       <li>Total:{total}</li>
-      <li>Positive feedback:{positivePercentage}</li>
+      <li>Positive feedback:{positivePercentage}%</li>
     </ul>
   ) : (
     <Notification />
